d54028870784d43b1e27d5cfbec9592b818ca4e2
pgb2100 <pgb2100@gmail.com>
2022-10-31 22:39:56 -0400

    update_final_versions

--- NN_Drug.py
+++ NN_Drug.py
@@ -35,41 +35,77 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import roc_curve,auc
 from sklearn.metrics import roc_auc_score
+from sklearn.svm import SVC
 
 def main():
     # storing the data
-    dataframe=pd.read_csv(file)
+    dfa=pd.read_csv(file_IC50)
+    dfb=pd.read_csv(file_gene)
+
+    Cetuximab = pd.concat([dfa, dfb], ignore_index=True, sort =False)
 
     # let's check the number of samples and features
     # note:the last column contain the labels. it is not considered as a feature
 
-    print(dataframe.shape)
+    print(dfa.shape)
+    print(dfb.shape)
+    print(dfb['Cancer_Type'].value_counts())
+
+    #let's check some of the columns 
+    print(dfa.columns[0:15])
+    print(dfb.columns [0:14])
+
+    #lets check the name of the columns of this dataframe
+    dfb.columns[0:5]
+    dfa.columns[0:1]
+
+    #check for missing values
+    datanul=dfb.isnull().sum()
     g=[i for i in datanul if i>0]
 
     print('columns with missing values:%d'%len(g))
+    
+    #Data Exploration & Cleaning
+    #Data cleaning is very important in machine learning as it makes our data to be of a high quality for better results.
 
+    #Here we will check for Null values and remove them for greater accuracy\
+    #In the exploration stage we will see the structure of our data, the columns, rows and their numbers
+    #How to check the rows of your data. By placing a number within the () you can control how much you can see.
+    dfa.head(3)
+    dfb.head(5)
+
+    #New Section
     # Checking for any null values in our dataset
-    dataframe.isnull().sum()
-
+    dfb.isnull().sum()
+    
+    #Dropping the null values
     # Dropping The rows that have the null values so our data may be accurate as possible
-    dataframe = dataframe.dropna(axis=0, how = "any", inplace=False)
-    dataframe
+    dfa = dfa.dropna(axis=0, how = "any", inplace=False)
+    print(dfb)
 
     # Checking if the Null Values have been dropped
-    dataframe.isnull().any()
+    dfa.isnull().any()
 
+    #Checking for the drugs that our data set contains
+    dfa["IC50"].value_counts()
+    dfb["gene_1000"].value_counts()
+    
+    #Data preprocesing
+    #This is done to put the data in an appropriate format before modelling
+    # in our data we will used= the following columns to develop our model that will be assigned to x and y variables
+    X=dfb[["gene_1000" ]].values
+    y= dfa[["IC50"]].values
 
-    dataframe["Drug Name"].value_counts()
+    #Encode labels & Data Splitting
+    #The labels for this data are categorical and we therefore have to convert them to numeric forms. This is referred to as encoding. Machine learning models usually require input data to be in numeric forms.
+    #Data is split into three: training, validation and test sets
+    #validation set is used for evaluating the model during training.
+    #training set is used for training
+    #test set is used to test the model after training and tuning has been completed.
 
-    # In our data we will use the following columns to develop our model that will be assigned to x and y variables
+    # let's encode target labels (y) with values between 0 and n_classes-1.
+    # we will be using LabelEncoder to perform the encoding
 
-
-    X=dataframe[["Drug ID"]].values
-    y= dataframe[["IC50"]].values
-
-    #Encode labels
-    #The labels for this data are categorical and we therefore have to convert them to numeric forms. 
-    #This is referred to as encoding. Machine learning models usually require input data to be in numeric forms.
     label_encoder=LabelEncoder()
     label_encoder.fit(X)
     y=label_encoder.transform(X)
@@ -78,77 +114,86 @@ def main():
     nclasses=np.unique(X).shape[0]
     nclasses
 
-    # split data into training,validation and test sets
+    #split data into training,validation and test sets
+    X_train,X_test,y_train,y_test=train_test_split(X,y, test_size=0.08,random_state=1)
 
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.8,random_state=1)
-
-    # split the training set into two (training and validation)
-    X_train, X_val, y_train, y_val = train_test_split(X_train,y_train,test_size=0.8)
+    #split the training set into two (training and validation)
+    X_train, X_val, y_train, y_val = train_test_split(X_train,y_train,test_size=0.08)
+    
+    # check the shape of X_train and y_train
+    X_train.shape, y_train.shape
 
     #Data Normalization
     #Data normalization is done so the values are in the same range to improve model performance and avoid bias.
 
-    min_max_scaler=MinMaxScaler()
-    X_train=min_max_scaler.fit_transform(X_train)
-    X_val=min_max_scaler.fit_transform(X_val)
-    X_test=min_max_scaler.fit_transform(X_test)
-    
-    # define model
-    model = Sequential()
+    #split data into training,validation and test sets
 
-    # hidden layer 1
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.02)
+
+    #split the training set into two (training and validation)
+    X_train, X_val, y_train, y_val = train_test_split(X_train,y_train,test_size=0.02)
+
+    #Building Neural Network
+    #define model
+    model = keras.Sequential()
+
+    #hidden layer 1
     model.add(Dense(40, input_dim=X_train.shape[1], activation='relu'))
 
-    # hidden layer 2
+    #hidden layer 2
     model.add(Dense(20, activation='relu'))
 
-    # output layer
+    #output layer
     model.add(Dense(nclasses, activation='softmax'))
 
-    # define optimizer and learning rate. We will use Adam optimizer
+    #define optimizer and learning rate. We will use Adam optimizer
     opt_adam = keras.optimizers.Adam(learning_rate=0.001)
 
     model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=opt_adam, metrics=[keras.metrics.SparseCategoricalAccuracy()])
-
     # fit the model to the training data
     # the higher the epoch value the higher the accuracy but the longer it takes to execute. 
     # An epoch means training the neural network with all the training data for one cycle. 
     # In an epoch, we use all of the data exactly once. A forward pass and a backward pass together are counted as one pass: 
     # An epoch is made up of one or more batches, where we use a part of the dataset to train the neural network.
-    history = model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=30,epochs=32, verbose=1)
+
+    history = model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=32,epochs=64, verbose=1)
 
     predictions = model.predict(X_test)
     _, accuracy = model.evaluate(X_test, y_test, verbose=0)
+    print(accuracy)
 
-    accuracy
-
-    predictions[0]
+    print(predictions[0])
 
     np.argmax(predictions)
 
     # Get the predictions for samples in the test set. You can change by alter the number within the []
-    for index,entry in enumerate(predictions[0:30,:]):
+    for index,entry in enumerate(predictions[0:10,:]):
         print('predicted:%d ,actual:%d'%(np.argmax(entry),y_test[index]))
 
+    
     # Summarize history for accuracy
+    plt.clf()
     plt.plot(history.history['sparse_categorical_accuracy'])
     plt.plot(history.history['val_sparse_categorical_accuracy'])
     plt.title('model performance')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='lower right')
-    plt.show()
+    plt.savefig("history_for_accuracy.png")
+
 
     # Summarize history for loss
+    plt.clf()
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='lower right')
-    plt.show()
+    plt.savefig("history_for_loss.png")
 
 if __name__ == '__main__':
     # read data directly from my home computer or GDRIVE
-    file=sys.argv[1]
+    file_IC50 = sys.argv[1]
+    file_gene = sys.argv[2]
     main()
